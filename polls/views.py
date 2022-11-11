6f9b1cfc87e3d627bf02e470f66fed870bea0063
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-11-11 15:56:24 +0530

    Create detail and result view for polls

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    choice = get_object_or_404(Choice, pk=question_id)
+    return render(request, 'polls/results.html', {'choice' : choice})
+
+def vote(request, question_id):
+    vote = get_object_or_404(Choice.votes)
+    return render(request, 'polls/vote.html', {'vote' : vote} )
